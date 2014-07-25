c613ef8e0b2802353280959ab0d5901aa1ef1f1c
lawrencekang <lawrenckang@gmail.com>
2014-07-24 21:49:18 -0700

    sprint two initial commit

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,48 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    //adding to tail, add new node as list.tail
+    //update the null value from the previous list.tail
+    if(list.head === null){
+      list.head = makeNode(value);
+      list.tail = list.head
+    } else {
+      var link = list.tail;
+      list.tail = makeNode(value);
+      link.next = list.tail;
+    };
   };
 
   list.removeHead = function(){
+    //remove first thing we added - set to list.head
+    //once list.head is deleted, next node should become list.head
+    //previous list.head.next is the next node that should become list.head
+    var previousHead = list.head.value;
+    var nextHead = list.head.next;
+    delete list.head;
+    list.head = nextHead;
+    return previousHead;
+
   };
 
-  list.contains = function(target){
+  list.contains = function(target, obj){
+    //check if the target is in any of the nodes
+    // list.head.value === target
+    // if it doesn't => move to list.head.next (unless .next === null)
+    // list.head.next.value === target
+    // if it doesn't => move to list.head.next.next (unless .next === null)
+    // list.head.next.next.value == target
+    // if it doesn't => move to list.head.next.next.next (unless .next === null)
+
+    obj = obj || list.head;
+    if(obj.value === target){
+      return true;
+    } else if(obj.next === null) {
+      return false;
+    } else {
+        return list.contains(target, obj.next);
+    }
+
   };
 
   return list;
